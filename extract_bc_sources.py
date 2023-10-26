65c1dce0750a844d2b682c2a4eead20764abd2da
Shank <shashanksharma21@gmail.com>
2023-10-26 15:51:21 -0400

    extract_bc_sources: add cache for dependencies

--- extract_bc_sources.py
+++ extract_bc_sources.py
@@ -67,12 +67,12 @@ for pkgs in packages_available:
     reverse_dependencies = []
     dependency_list = p.dependency_map[pkgs]
     for dependencies in dependency_list:
-        if dependencies in checked_dep_cache:
-            continue
+        if dependencies not in checked_dep_cache:
+            subprocess.call(["sudo apt -yq install", str(dependencies)], shell=True)
+            checked_dep_cache.add(dependencies)
         # install all reverse dependencies might be too slow (not necessary), uncomment if needed
-        #for reverse_deps in p.reverse_dependency_map[dependencies]:
-        #    subprocess.call(['sudo apt -yq install', str(reverse_deps)], shell=True)        
-        subprocess.call(['sudo apt -yq install', str(dependencies)], shell=True)
+        # for reverse_deps in p.reverse_dependency_map[dependencies]:
+        #    subprocess.call(['sudo apt -yq install', str(reverse_deps)], shell=True)
         reverse_dependencies.extend(p.reverse_dependency_map[dependencies])
     
     for lib in reverse_dependencies:
